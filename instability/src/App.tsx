441675853159161f7d7214a79991c2a2690adff3
Will Brammer <williamedwardbrammer@gmail.com>
2024-03-30 12:26:35 +0900

    feat: implement component for upload
    Made a component that allows for an upload of only one file and one photo type

--- instability/src/App.tsx
+++ instability/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import LocalUpload from "./components/LocalUpload";
 
 function App() {
   return (
     <>
       <div className="bg-blue-500">Kai is a cutie</div>
+      <LocalUpload />
     </>
   );
 }
---
+++ instability/src/components/LocalUpload.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+
+const LocalUpload = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [preview, setPreview] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    if (!selectedFile) {
+      setPreview(undefined);
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(selectedFile);
+    setPreview(objectUrl);
+
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [selectedFile]);
+
+  const onSelectFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setSelectedFile(null);
+      return;
+    }
+
+    const file = e.target.files[0];
+    if (!file.type.startsWith("image/")) {
+      window.alert("wrong file type!!!!!");
+      return;
+    }
+
+    setSelectedFile(file);
+  };
+
+  return (
+    <div>
+      <input type="file" accept="image/*" onChange={onSelectFile} />
+      {selectedFile && <img src={preview} alt="Selected preview" />}
+    </div>
+  );
+};
+
+export default LocalUpload;
