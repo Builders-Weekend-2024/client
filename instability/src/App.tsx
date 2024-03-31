80c328fb13cf1c6d6eba453939f1002eadfbedbd
chadgrover <93070367+chadgrover@users.noreply.github.com>
2024-03-31 15:06:30 +0900

    style: Clean up image modal

--- instability/src/App.tsx
+++ instability/src/App.tsx
@@ -3,7 +3,7 @@ import LocalUpload from "./components/LocalUpload";
 
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center h-[100vh] w-[100vw] gap-6">
+    <main className="flex flex-col items-center justify-center gap-6">
       <LocalUpload />
     </main>
   );
--- instability/src/components/GenerateImagePreview.tsx
+++ instability/src/components/GenerateImagePreview.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 interface GeneratedImagePreviewProps {
-  base64Response: string; // Base64 encoded image data
+  base64Response: string | null; // Base64 encoded image data
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>; // Function to toggle modal display
 }
 
@@ -14,19 +14,20 @@ const GeneratedImagePreview: React.FC<GeneratedImagePreviewProps> = ({
   };
 
   return (
-    <div className="fixed inset-0 z-50 flex justify-center items-center bg-gray-900 bg-opacity-75">
-      <div className="max-w-3/4 h-auto">
+    <div className="fixed inset-0 z-50 flex justify-center items-center bg-opacity-75 flex-col">
+      <div className="max-w-3/4 h-[75%] flex gap-2 flex-col justify-center items-center">
+        <p className="text-white font-bold">Your Generated Image</p>
         <img
           src={`data:image/jpeg;base64,${base64Response}`}
           alt="Preview"
-          className="max-w-full max-h-full"
+          className="max-w-full max-h-full border-white border-2 border-md"
         />
       </div>
       <button
         onClick={closeModal}
         className="absolute top-0 right-0 m-4 text-white"
       >
-        Close
+        X
       </button>
     </div>
   );
